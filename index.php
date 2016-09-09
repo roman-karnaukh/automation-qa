8f2d73cbecd7d868e36ba7ec68f6ab99adfb04b5
Roman Karnaukh <roman.karnaukh@gmail.com>
2016-09-09 14:54:27 +0300

    f

--- index.php
+++ index.php
@@ -15,9 +15,6 @@
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
 	<body>
-		<php?
-		echo "it is PHP";
-		?>
 		<!-- Wrapper-->
 			<div id="wrapper">
 
